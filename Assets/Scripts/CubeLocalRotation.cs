8a1f89abc5e1fdaeadfcb504b80b60810e3bcdc8
Domaq <abay.adk@gmail.com>
2017-02-25 22:24:14 +0400

    remove tabs

--- Assets/Scripts/CubeLocalRotation.cs
+++ Assets/Scripts/CubeLocalRotation.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class CubeLocalRotation : MonoBehaviour {
     Quaternion originalLocalRotation;
     float angle = 0f;
-    float rotation_speed = 15f;
-	// Use this for initialization
-	void Start () {
+    public readonly float rotation_speed = 15f;
+    // Use this for initialization
+    void Start () {
         originalLocalRotation = transform.localRotation;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButton(1))
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButton(1))
         {
             angle -= rotation_speed * Input.GetAxis("Mouse X");
             if (angle < -360f)
