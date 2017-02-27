2910177c66d163228742a14b2048ce53e97483cc
Domaq <abay.adk@gmail.com>
2017-02-27 19:03:14 +0400

    rework of cube rotation

--- Assets/Scripts/CubeLocalRotation.cs
+++ Assets/Scripts/CubeLocalRotation.cs
@@ -1,26 +1,32 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeLocalRotation : MonoBehaviour {
-    Quaternion originalLocalRotation;
-    float angle = 0f;
-    public readonly float rotation_speed = 15f;
+public class CubeLocalRotation : MonoBehaviour
+{
+    float rotx = 0f;
+    float roty = 0f;
+    public readonly float rotation_speed_x = 10f;
+    public readonly float rotation_speed_y = 10f;
+
+    Vector3 upFromWorld;
+    Vector3 rightFromWorld;
     // Use this for initialization
-    void Start () {
-        originalLocalRotation = transform.localRotation;
+    void Start()
+    {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         if (Input.GetMouseButton(1))
         {
-            angle -= rotation_speed * Input.GetAxis("Mouse X");
-            if (angle < -360f)
-                angle += 360f;
-            if (angle > 360f)
-                angle -= 360f;
-            transform.localRotation = originalLocalRotation * Quaternion.Euler(0, angle, 0);
-        }
+            rotx = Input.GetAxis("Mouse X") * rotation_speed_x;
+            roty = Input.GetAxis("Mouse Y") * rotation_speed_y;
+
+            upFromWorld = transform.InverseTransformVector(Vector3.up);
+            rightFromWorld = transform.InverseTransformVector(Vector3.right);
+            
+            transform.localRotation *= Quaternion.AngleAxis(-rotx, upFromWorld) * Quaternion.AngleAxis(roty, rightFromWorld);        }
     }
 }
