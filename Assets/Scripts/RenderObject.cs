1a1da85f335443416b17442f3aa861adfee7f602
Natalia Smirnova <Natalia_Smirnova@epam.com>
2017-03-10 09:53:37 +0300

    Rough rendering to texture

--- Assets/Scripts/RenderObject.cs
+++ Assets/Scripts/RenderObject.cs
@@ -1,31 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RenderObject : MonoBehaviour {
 
     public GameObject renderObject;
-
-    private RenderTexture renderTexture;
-    private SpriteRenderer spriteRenderer;
-
+    private RawImage image;
+    private RenderTexture rt;
+    
     void Start () {
-        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+      image = GetComponent<RawImage>();
+      rt = RenderTexture.GetTemporary(Screen.width, Screen.height);
     }
 	
-	void Update () {
-        // texture
-        renderTexture = RenderTexture.GetTemporary(Screen.width, Screen.height);
-        Graphics.SetRenderTarget(renderTexture);
-
-        spriteRenderer.material.SetPass(0);
+	  void Update () {
+       
+        Graphics.SetRenderTarget(rt);
 
         Mesh objectMesh = renderObject.GetComponent<MeshFilter>().sharedMesh;
+        var renderer = renderObject.GetComponent<MeshRenderer>();
+        renderer.material.SetPass(0);
+
         Graphics.DrawMeshNow(objectMesh, objectMesh.vertices[0], Quaternion.identity);
 
-        spriteRenderer.material.mainTexture = renderTexture;
+        image.texture = rt;
 
         Graphics.SetRenderTarget(null);
-        RenderTexture.ReleaseTemporary(renderTexture);
     }
 }
