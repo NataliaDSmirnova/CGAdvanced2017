9ffdd59aed896df85dc8a024632084b1e8fc16a6
MarinaIvanova239 <mar.ivanova@bk.ru>
2017-03-12 19:52:14 +0300

    fix local conflicts

--- Assets/Scripts/RenderObject.cs
+++ Assets/Scripts/RenderObject.cs
@@ -6,40 +6,26 @@ using UnityEngine.UI;
 public class RenderObject : MonoBehaviour {
 
     public GameObject renderObject;
-<<<<<<< HEAD
 
     private RenderTexture renderTexture;
     private SpriteRenderer spriteRenderer;
-    private int xSizeOfSprite;
-    private int ySizeOfSprite;
-
-    void Start () {
-        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
-        xSizeOfSprite = (int) spriteRenderer.sprite.border.x;
-        ySizeOfSprite = (int) spriteRenderer.sprite.border.y;
-=======
     private RawImage image;
     private RenderTexture rt;
     
     void Start () {
-      image = GetComponent<RawImage>();
-      rt = RenderTexture.GetTemporary(Screen.width, Screen.height);
->>>>>>> origin/master
+        image = GetComponent<RawImage>();
+        rt = RenderTexture.GetTemporary(Screen.width, Screen.height);
     }
 	
-	  void Update () {
+	void Update () {
        
         Graphics.SetRenderTarget(rt);
 
         Mesh objectMesh = renderObject.GetComponent<MeshFilter>().sharedMesh;
-<<<<<<< HEAD
-        Graphics.DrawMeshNow(objectMesh, renderObject.transform.position, Quaternion.identity);
-=======
         var renderer = renderObject.GetComponent<MeshRenderer>();
         renderer.material.SetPass(0);
 
         Graphics.DrawMeshNow(objectMesh, objectMesh.vertices[0], Quaternion.identity);
->>>>>>> origin/master
 
         image.texture = rt;
 
