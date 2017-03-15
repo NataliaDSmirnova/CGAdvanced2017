e04cac556c568d7b94d9102a0dc9b5c274f3503c
MarinaIvanova239 <mar.ivanova@bk.ru>
2017-03-15 20:02:29 +0300

    fix rendering script

--- Assets/Scripts/RenderObject.cs
+++ Assets/Scripts/RenderObject.cs
@@ -15,11 +15,11 @@ public class RenderObject : MonoBehaviour {
     void Start () {
         // get raw image from scene (see RTSprite)
         image = GetComponent<RawImage>();
-    }
-	
-	void Update () {
         // create temprorary texture of screen size
         renderTexture = RenderTexture.GetTemporary(Screen.width, Screen.height);
+    }
+	
+	void OnRenderObject () {
         // set our temprorary texture as target for rendering
         Graphics.SetRenderTarget(renderTexture);
 
@@ -30,21 +30,14 @@ public class RenderObject : MonoBehaviour {
         // activate first shader pass for our renderer
         renderer.material.SetPass(0);
 
-        // render from camera to texture
-        // mainCamera.targetTexture = renderTexture;
-        // mainCamera.Render();
-
         // draw mesh of input object to render texture
-        Graphics.DrawMeshNow(objectMesh, renderer.localToWorldMatrix * mainCamera.worldToCameraMatrix * mainCamera.projectionMatrix);
+        Graphics.DrawMeshNow(objectMesh, 
+            renderObject.transform.localToWorldMatrix * mainCamera.worldToCameraMatrix * mainCamera.projectionMatrix);
 
         // set texture of raw image equals to our render texture
         image.texture = renderTexture;
 
-        //mainCamera.targetTexture = null;
-
         // render again to backbuffer
         Graphics.SetRenderTarget(null);
-        // release texture
-        RenderTexture.ReleaseTemporary(renderTexture);
     }
 }
