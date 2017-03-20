7b8d8035bbb5db51f780f7667409ae521d5219af
Artyom Tetiukhin <artemtetiukhin@yandex.com>
2017-03-20 18:38:33 +0300

    Fix bug

--- Assets/Scripts/ChangeMaterial.cs
+++ Assets/Scripts/ChangeMaterial.cs
@@ -5,25 +5,19 @@ using UnityEngine.UI;
 
 public class ChangeMaterial : MonoBehaviour
 {
-    public Button button;
-    public Renderer cube;
     public Material otherMaterial;
-    public Shader otherShader;
 
+    private Renderer cube;
     private Material originalMaterial;
-    private Shader originalShader;
 
     void Start()
     {
-        var btnChangeMaterial = button.GetComponent<Button>();
-        btnChangeMaterial.onClick.AddListener(ChangeMaterialOnClick);
+        cube = GetComponent<Renderer>();
         originalMaterial = cube.material;
-        originalShader = cube.material.shader;
     }
 
-    void ChangeMaterialOnClick()
+    public void ChangeMaterialOnClick()
     {
-        //cube.material = cube.material != originalMaterial ? originalMaterial : otherMaterial;
-        cube.material.shader = cube.material.shader != originalShader ? originalShader : otherShader;
+        cube.material = cube.sharedMaterial != originalMaterial ? originalMaterial : otherMaterial;
     }
 }
