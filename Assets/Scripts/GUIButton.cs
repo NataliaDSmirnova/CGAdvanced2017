985631e3a407acba2d022d5fe86fcebef632a87b
Artyom Tetiukhin <artemtetiukhin@yandex.com>
2017-03-23 19:56:03 +0300

    Fix bug with button

---
+++ Assets/Scripts/GUIButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIButton : MonoBehaviour
+{
+    private const float eps = 0;
+
+    void Update()
+    {
+        var button = transform.GetComponent<RectTransform>();
+        button.localPosition = new Vector3(
+            (Screen.width - button.sizeDelta[0]) / 2 - eps,
+            -(Screen.height - button.sizeDelta[1]) / 2 - eps,
+            0);
+    }
+}
