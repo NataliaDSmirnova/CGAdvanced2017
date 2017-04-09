4b51ea61b2d40aa6c0a91ade51cb727644c726f0
Kamila1625 <kamila.de.bua.tresy@gmail.com>
2017-04-09 20:13:55 +0300

    Add dropdown list to UI (with small bug)

---
+++ Assets/Scripts/DropDownLog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownLog : MonoBehaviour {
+
+    public UnityEngine.UI.Dropdown d = null;
+
+    void Log(int arg)
+    {
+        Debug.Log(d.value.ToString());
+        Debug.Log("Work");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        d.onValueChanged.AddListener(Log);
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
