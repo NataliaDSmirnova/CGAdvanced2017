104ddfd7025255a83c5d299d762b94aa9f2ca12f
Дарья Красильникова < >
2017-04-10 18:32:35 +0300

    Clip plane

---
+++ Assets/Scripts/ClipPlane.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClipPlane : MonoBehaviour
+{
+
+    // input objects
+    public GameObject renderObject;
+    public Slider slider;
+    public Shader shader;
+
+    private float clipX;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clipX = slider.value - 0.5f;
+    }
+
+    void OnRenderObject()
+    {
+        Graphics.SetRenderTarget(null);
+
+        // get mesh from input object
+        Mesh objectMesh = renderObject.GetComponent<MeshFilter>().sharedMesh;
+        if (objectMesh == null)
+        {
+            Debug.Log("Can't get mesh from input object");
+            return;
+        }
+
+        // get mesh renderer from input object
+        var renderer = renderObject.GetComponent<MeshRenderer>();
+        if (renderer == null)
+        {
+            Debug.Log("Can't get mesh renderer from input object");
+            return;
+        }
+
+        // activate first shader pass for our renderer
+        renderer.material.shader = shader;
+        renderer.material.SetFloat("_ClipX", clipX);
+        renderer.material.SetPass(0);
+
+        // draw mesh of input object
+        Graphics.DrawMeshNow(objectMesh, renderObject.transform.localToWorldMatrix);
+    }
+}
---
+++ Assets/Shaders/ClipPlaneShader.shader
@@ -0,0 +1,66 @@
+﻿Shader "CGA/ClipPlaneShader" {
+
+	Properties{
+		_Opacity("opacity", Range(0, 1)) = 0.6
+		_ClipX("clipX", float) = 1.0
+	}
+
+		SubShader{
+		Tags
+	{
+		"Queue" = "Transparent"
+		"RenderType" = "Transparent"
+	}
+
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+	{
+		CGPROGRAM
+
+#include "UnityCG.cginc"
+
+#pragma vertex vert
+#pragma fragment frag
+
+		float _Opacity;
+	float _ClipX;
+
+	struct VertexInput
+	{
+		float4 vertex : POSITION;
+		float3 normal : NORMAL;
+	};
+
+	struct VertexOutput
+	{
+		float4 vertex : SV_POSITION;
+		float4 wpos: TEXTCOORD0;
+		fixed4 color : COLOR;
+	};
+
+	VertexOutput vert(VertexInput vertexInput)
+	{
+		VertexOutput vertexOutput;
+
+		vertexOutput.vertex = mul(UNITY_MATRIX_MVP, vertexInput.vertex);
+		vertexOutput.wpos = mul(unity_ObjectToWorld, vertexInput.vertex);
+		float3 vertexNormal = abs(vertexInput.normal);
+		vertexOutput.color = float4(vertexNormal.x, vertexNormal.y, vertexNormal.z, _Opacity);
+		return vertexOutput;
+	}
+
+	fixed4 frag(VertexOutput vertexOutput) : SV_Target
+	{
+		if (vertexOutput.wpos.x < _ClipX)
+		discard;
+	return vertexOutput.color;
+	}
+
+		ENDCG
+	}
+	}
+
+		FallBack "VertexLit"
+
+}
