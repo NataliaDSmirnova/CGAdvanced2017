10d9c60bd64d57bc5cb94d61a0c1de25828f2a39
Natalia Smirnova <Natalia_Smirnova@epam.com>
2017-04-13 11:42:36 +0300

    Use appropriate name

--- Assets/Scripts/DebugTextureSwitcher.cs
+++ Assets/Scripts/DebugTextureSwitcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RenderObject : MonoBehaviour
+public class DebugTextureSwitcher : MonoBehaviour
 {
     // private objects
     private RawImage image;
