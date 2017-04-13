c884fff8bb0724fa8f14cdb287642a7add55e5a9
Natalia Smirnova <Natalia_Smirnova@epam.com>
2017-04-13 11:36:01 +0300

    Reorganize rendering back-, front- textures
    Rendering is controlled in camera component FronBackTextureCreator . Sprite only uses textures.

---
+++ Assets/Scripts/BackFrontTextureCreator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class BackFrontTextureCreator : MonoBehaviour
+{
+    // input objects
+    public GameObject renderObject;
+    public Material frontMaterial;
+    public Material backMaterial;
+    public RenderTexture FrontTexture { get { return frontTexture; } }
+    public RenderTexture BackTexture { get { return backTexture; } }
+
+    // private objects
+    private RenderTexture frontTexture;
+    private RenderTexture backTexture;
+    private CommandBuffer buffer;
+    
+    void Start()
+    {
+        frontTexture = RenderTexture.GetTemporary(Screen.width, Screen.height, 1, RenderTextureFormat.ARGBFloat);
+        backTexture = RenderTexture.GetTemporary(Screen.width, Screen.height, 1, RenderTextureFormat.ARGBFloat);
+
+        // get mesh from input object
+        buffer = new CommandBuffer();
+        GetComponent<Camera>().AddCommandBuffer(CameraEvent.BeforeForwardOpaque, buffer);
+    }
+
+    void OnPreRender()
+    {
+        // get mesh from input object
+        var mesh = renderObject.GetComponent<MeshFilter>().sharedMesh;
+        if (mesh == null)
+        {
+            Debug.Log("Can't get mesh from input object");
+            return;
+        }
+
+        // form command buffer 
+        buffer.Clear();
+        if (frontTexture  != null && frontMaterial)
+        {
+            buffer.SetRenderTarget(frontTexture);
+            buffer.ClearRenderTarget(true, true, new Color(0f, 0f, 0f));
+            buffer.DrawMesh(mesh, renderObject.transform.localToWorldMatrix, frontMaterial);
+        }
+        if (backTexture != null && backMaterial)
+        {
+            buffer.SetRenderTarget(backTexture);
+            buffer.ClearRenderTarget(true, true, new Color(0f, 0f, 0f));
+            buffer.DrawMesh(mesh, renderObject.transform.localToWorldMatrix, backMaterial);
+        }
+    }
+}
--- Assets/Scripts/RenderObject.cs
+++ Assets/Scripts/RenderObject.cs
@@ -5,20 +5,17 @@ using UnityEngine.UI;
 
 public class RenderObject : MonoBehaviour
 {
-    // input objects
-    public GameObject renderObject;
-
     // private objects
     private RawImage image;
     private bool renderFront = true;
-    private RenderTexture renderTexture;
+    private BackFrontTextureCreator textureCreator;
 
     void Start()
     {
         // get raw image from scene (see RTSprite)
         image = GetComponent<RawImage>();
-        // create temprorary texture of screen size
-        renderTexture = RenderTexture.GetTemporary(Screen.width, Screen.height);
+        // get texture creator from camera
+        textureCreator = Camera.main.GetComponent<BackFrontTextureCreator>();
     }
 
     void Update()
@@ -30,89 +27,23 @@ public class RenderObject : MonoBehaviour
         // fix position of image in left lower corner
         image.rectTransform.anchoredPosition = new Vector2((imageWidth - Screen.width) / 2,
                                                            (imageHeight - Screen.height) / 2);
+
+        // renew texture 
+        SetTexture();
     }
 
-    void OnRenderObject()
-    {
-        // set our temprorary texture as target for rendering
-        Graphics.SetRenderTarget(renderTexture);
-        // clear render texture
-        GL.Clear(false, true, new Color(0f, 0f, 0f));
-
-        if (renderObject == null)
-        {
-            Debug.Log("Object for rendering is not set");
-            return;
-        }
-
-        // get mesh from input object
-        Mesh objectMesh = renderObject.GetComponent<MeshFilter>().sharedMesh;
-        if (objectMesh == null)
-        {
-            Debug.Log("Can't get mesh from input object");
-            return;
-        }
-
-        // if render back face, reverse mesh
-        if (!renderFront)
-        {
-            objectMesh = ReverseObjectMesh(objectMesh);
-        }
-
-        // get mesh renderer from input object
-        var renderer = renderObject.GetComponent<MeshRenderer>();
-        if (renderer == null)
-        {
-            Debug.Log("Can't get mesh renderer from input object");
-            return;
-        }
-
-        // activate first shader pass for our renderer
-        renderer.material.SetPass(0);
-
-        // draw mesh of input object to render texture
-        Graphics.DrawMeshNow(objectMesh, renderObject.transform.localToWorldMatrix);
-
-        // set texture of raw image equals to our render texture
-        image.texture = renderTexture;
-
-        // render again to backbuffer
-        Graphics.SetRenderTarget(null);
-    }
-
-    public void RenderObjectOnValueChanged()
+    public void Switch()
     {
         renderFront = !renderFront;
     }
 
-    Mesh ReverseObjectMesh(Mesh objectMesh)
+    private void SetTexture()
     {
-        var newObjectMesh = new Mesh();
-        var objectNormals = objectMesh.normals;
-        var newNormals = new Vector3[objectNormals.Length];
-
-        // switch normals direction
-        for (var j = 0; j < objectNormals.Length; j++)
+        if (!textureCreator)
         {
-            newNormals[j] = -objectNormals[j];
+            Debug.Log("Texture creator is null");
+            return;
         }
-
-        // set new order for vertices in triangles
-        var objectTriangles = objectMesh.triangles;
-        var newTriangles = new int[objectTriangles.Length];
-        for (var i = 0; i < objectTriangles.Length; i += 3)
-        {
-            newTriangles[i] = objectTriangles[i];
-            newTriangles[i + 1] = objectTriangles[i + 2];
-            newTriangles[i + 2] = objectTriangles[i + 1];
-        }
-
-        // set all variables to new object mesh
-        newObjectMesh.vertices = objectMesh.vertices;
-        newObjectMesh.uv = objectMesh.uv;
-        newObjectMesh.normals = newNormals;
-        newObjectMesh.triangles = newTriangles;
-
-        return newObjectMesh;
+        image.texture = renderFront ? textureCreator.FrontTexture : textureCreator.BackTexture;
     }
 }
