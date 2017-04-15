006f936508db37ab1b7b983a6da0cc288a8416bf
Kamila1625 <kamila.de.bua.tresy@gmail.com>
2017-04-15 22:24:16 +0300

    DropDownList with names of pvm-files + names of files in log

--- Assets/Scripts/DropDownLog.cs
+++ Assets/Scripts/DropDownLog.cs
@@ -5,21 +5,44 @@ using UnityEngine;
 public class DropDownLog : MonoBehaviour {
 
     public UnityEngine.UI.Dropdown d;
+    public string standartNamePVM;
 
     void Log(int arg)
     {
-        Debug.Log(d.value.ToString());
-        Debug.Log("Work");
+        string optionselected = d.captionText.text;
+        Debug.Log(optionselected);
     }
 
     // Use this for initialization
     void Start()
     {
-      if (d != null)
-      {
-        d.onValueChanged.AddListener(Log);
-        Debug.Log("Hello");
-      }
+        if (d != null)
+        {
+            d.onValueChanged.AddListener(Log);
+            Debug.Log("Hello");
+
+
+           // List<string> PVMInResources = new List<string>();
+            string pathResources = "Assets/Resources/";
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(pathResources);
+            System.IO.FileInfo[] files = dir.GetFiles("*.pvm");
+            int dValue = 0;
+            int idx = 0;
+            foreach (System.IO.FileInfo f in files)
+            {
+                string tempName = f.Name;
+                if (tempName == standartNamePVM)
+                {
+                    dValue = idx;
+                }
+                d.options.Add(new UnityEngine.UI.Dropdown.OptionData(tempName));
+                idx = idx + 1;
+
+            }
+            d.value = dValue;
+            d.Select();
+            d.RefreshShownValue();
+        }
     }
 
     // Update is called once per frame
@@ -29,3 +52,4 @@ public class DropDownLog : MonoBehaviour {
     }
 
 }
+
